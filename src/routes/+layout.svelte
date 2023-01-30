60baa79a47028e4e61dd0e0ff70dda0d2f2bce8d
iamgabrieltv <gabrielsarcevic1@gmail.com>
2023-01-30 19:31:50 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+ import "../app.css";
+</script>
+
+<div class="navbar bg-neutral">
+ <div class="navbar-start">
+   <div class="dropdown">
+     <label tabindex="0" class="btn btn-ghost btn-circle">
+       <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+     </label>
+     <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+       <li><a>Homepage</a></li>
+       <li><a>Portfolio</a></li>
+       <li><a>About</a></li>
+     </ul>
+   </div>
+ </div>
+ <div class="navbar-center">
+   <a class="normal-case text-xl">GS Chat</a>
+ </div>
+ <div class="navbar-end">
+  <button class="btn btn-square btn-ghost">
+   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+ </button>
+ </div>
+</div>
+
+<slot />
