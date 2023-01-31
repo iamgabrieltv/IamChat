10753ceee1f950094d10075865082f199cf56e35
iamgabrieltv <gabrielsarcevic1@gmail.com>
2023-01-31 21:49:47 +0100

    Why does Supabase have to be so hard to implement

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -1,17 +1,41 @@
-<div class="sticky top-0 z-30 flex h-16 w-full justify-center bg-opacity-90 backdrop-blur transition-all duration-100 bg-base-100 text-base-content">
-  <nav class="navbar w-full bg-neutral">
-    <div class="navbar-start">
-      <label for="my-drawer-2" class="btn btn-square btn-ghost drawer-button lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-      </label>
-    </div>
-    <div class="navbar-center">
-      <a class="normal-case text-xl">GS Chat</a>
-    </div>
-    <div class="navbar-end">
-      <button class="btn btn-square btn-ghost">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-      </button>
-    </div>
-  </nav>
+<div
+	class="sticky top-0 z-30 flex h-16 w-full justify-center bg-opacity-90 backdrop-blur transition-all duration-100 bg-base-100 text-base-content"
+>
+	<nav class="navbar w-full bg-neutral">
+		<div class="navbar-start">
+			<label for="my-drawer-2" class="btn btn-square btn-ghost drawer-button lg:hidden">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					class="inline-block w-5 h-5 stroke-current"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4 6h16M4 12h16M4 18h16"
+					/></svg
+				>
+			</label>
+		</div>
+		<div class="navbar-center">
+			<a class="normal-case text-xl">GS Chat</a>
+		</div>
+		<div class="navbar-end">
+			<button class="btn btn-square btn-ghost">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					class="inline-block w-5 h-5 stroke-current"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+					/></svg
+				>
+			</button>
+		</div>
+	</nav>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,26 @@
-<script>
- import "../app.css";
+<script lang="ts">
+	import '../app.css';
 
- import Navbar from "$lib/navbar.svelte"
+	import Navbar from '$lib/navbar.svelte';
 </script>
 
 <div class="drawer drawer-mobile">
-  <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-  <div class="drawer-content" style="scroll-behavior: smooth; scroll-padding-top: 5rem;">
-    <!-- Page content here -->
-    <Navbar />
+	<input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content" style="scroll-behavior: smooth; scroll-padding-top: 5rem;">
+		<!-- Page content here -->
+		<Navbar />
 
-    <div class="flex m-2 justify-center">
-      <slot />
-    </div>
-  </div> 
-  <div class="drawer-side">
-    <label for="my-drawer-2" class="drawer-overlay"></label> 
-    <ul class="menu p-4 w-80 bg-neutral text-base-content">
-      <!-- Sidebar content here -->
-      <h1 class="text-xl font-bold">DMs</h1>
-      <li><a>Sidebar Item 1</a></li>
-      <li><a>Sidebar Item 2</a></li>
-    </ul>
-  
-  </div>
+		<div class="flex m-2 justify-center">
+			<slot />
+		</div>
+	</div>
+	<div class="drawer-side">
+		<label for="my-drawer-2" class="drawer-overlay" />
+		<ul class="menu p-4 w-80 bg-neutral text-base-content">
+			<!-- Sidebar content here -->
+			<h1 class="text-xl font-bold">DMs</h1>
+			<li><a>Sidebar Item 1</a></li>
+			<li><a>Sidebar Item 2</a></li>
+		</ul>
+	</div>
 </div>
