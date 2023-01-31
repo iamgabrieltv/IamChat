e099a96a8bbd428fba93daf01e98a919c4ae8874
iamgabrieltv <gabrielsarcevic1@gmail.com>
2023-01-31 20:18:33 +0100

    Made stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,28 +1,27 @@
 <script>
  import "../app.css";
+
+ import Navbar from "$lib/navbar.svelte"
 </script>
 
-<div class="navbar bg-neutral">
- <div class="navbar-start">
-   <div class="dropdown">
-     <label tabindex="0" class="btn btn-ghost btn-circle">
-       <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-     </label>
-     <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-       <li><a>Homepage</a></li>
-       <li><a>Portfolio</a></li>
-       <li><a>About</a></li>
-     </ul>
-   </div>
- </div>
- <div class="navbar-center">
-   <a class="normal-case text-xl">GS Chat</a>
- </div>
- <div class="navbar-end">
-  <button class="btn btn-square btn-ghost">
-   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
- </button>
- </div>
-</div>
+<div class="drawer drawer-mobile">
+  <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content" style="scroll-behavior: smooth; scroll-padding-top: 5rem;">
+    <!-- Page content here -->
+    <Navbar />
 
-<slot />
+    <div class="flex m-2 justify-center">
+      <slot />
+    </div>
+  </div> 
+  <div class="drawer-side">
+    <label for="my-drawer-2" class="drawer-overlay"></label> 
+    <ul class="menu p-4 w-80 bg-neutral text-base-content">
+      <!-- Sidebar content here -->
+      <h1 class="text-xl font-bold">DMs</h1>
+      <li><a>Sidebar Item 1</a></li>
+      <li><a>Sidebar Item 2</a></li>
+    </ul>
+  
+  </div>
+</div>
